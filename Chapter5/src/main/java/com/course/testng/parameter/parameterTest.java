83d3b01f49ee6f23f4c76b806a02a85e091be41d
xinzhu221 <janewang221@sina.com>
2019-07-17 23:07:33 +0800

    modify

---
+++ Chapter5/src/main/java/com/course/testng/parameter/parameterTest.java
@@ -0,0 +1,13 @@
+package com.course.testng.parameter;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class parameterTest {
+
+    @Test
+    @Parameters({"name", "age"})
+    public void paraTest(String name, int age){
+        System.out.println("name = "+ name + ", age = " + age);
+    }
+}
