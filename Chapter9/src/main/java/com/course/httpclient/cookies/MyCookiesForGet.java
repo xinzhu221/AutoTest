763426e884625473cb4ebd11a4aba4a7551b95c8
xinzhu221 <janewang221@sina.com>
2019-07-25 22:03:10 +0800

    add get with test response cookies

--- Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
@@ -2,7 +2,6 @@ package com.course.httpclient.cookies;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.CookieStore;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -19,6 +18,7 @@ import java.util.ResourceBundle;
 public class MyCookiesForGet {
     private String url;
     private ResourceBundle bundle;
+    private CookieStore cookieStore;
     @BeforeTest
     public void beforeTest(){
         bundle = ResourceBundle.getBundle("application", Locale.CHINA);
@@ -27,7 +27,7 @@ public class MyCookiesForGet {
     @Test
     public void testGetCookies() throws IOException {
         String result;
-        String uri = bundle.getString("getCookies.uri");
+        String uri = bundle.getString("test.getCookies.uri");
         String testUrl = this.url + uri;
         HttpGet get = new HttpGet(testUrl);
         DefaultHttpClient client = new DefaultHttpClient();
@@ -35,13 +35,31 @@ public class MyCookiesForGet {
         result = EntityUtils.toString(response.getEntity(), Charset.defaultCharset());
         System.out.println(result);
         //获取cookies信息
-        CookieStore store = client.getCookieStore();
-        List<Cookie> cookieList =store.getCookies();
+        this.cookieStore = client.getCookieStore();
+        List<Cookie> cookieList = cookieStore.getCookies();
         for(Cookie cookie : cookieList){
             String name = cookie.getName();
             String value = cookie.getValue();
             System.out.println("name: " + name + ";value: " +value);
         }
+    }
+    @Test(dependsOnMethods = {"testGetCookies"})
+    public void testGetWithCookies() throws IOException {
 
+        String uri = bundle.getString("test.get.with.cookies");
+        String testUrl = this.url + uri;
+        HttpGet get = new HttpGet(testUrl);
+        DefaultHttpClient client = new DefaultHttpClient();
+        //设置cookies信息
+        client.setCookieStore(this.cookieStore);
+        HttpResponse response = client.execute(get);
+
+        //获取响应的状态码
+        int statusCode = response.getStatusLine().getStatusCode();
+        System.out.println("statusCode=" + statusCode);
+        if(statusCode == 200){
+            String result = EntityUtils.toString(response.getEntity(), Charset.defaultCharset());
+            System.out.println(result);
+        }
     }
 }
