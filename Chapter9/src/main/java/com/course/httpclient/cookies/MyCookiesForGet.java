a13c192a4884f8439d63c460fcfdbe263e0753f4
xinzhu221 <janewang221@sina.com>
2019-07-25 21:37:49 +0800

    add get uri from config

--- Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
@@ -1,4 +1,35 @@
 package com.course.httpclient.cookies;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class MyCookiesForGet {
+    private String url;
+    private ResourceBundle bundle;
+    @BeforeTest
+    public void beforeTest(){
+        bundle = ResourceBundle.getBundle("application", Locale.CHINA);
+        url = bundle.getString("test.url");
+    }
+    @Test
+    public void testGetCookies() throws IOException {
+        String result;
+        String uri = bundle.getString("getCookies.uri");
+        String testUrl = this.url + uri;
+        HttpGet get = new HttpGet(testUrl);
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(get);
+        result = EntityUtils.toString(response.getEntity(), Charset.defaultCharset());
+        System.out.println(result);
+    }
 }
